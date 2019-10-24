13875499455be8fdd695186a03641c89e964cb1b
Derrick Miller <derrick.f.miller@gmail.com>
2019-10-24 18:03:29 -0400

    removed unnecessary root project folder. cleaned up service structure for workshop-signup application components.

--- services/linux/workshop-signup-root/assets/scripts/02-ship.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker image push $DTR_HOST/dockersamples/hybrid-app-db
-docker image push $DTR_HOST/dockersamples/hybrid-app-web 
-docker image push $DTR_HOST/dockersamples/hybrid-app-web:v2 
-docker image push $DTR_HOST/dockersamples/hybrid-app-api:dotnet 
